819ddcfb42c4589b80902af6c776c23e7bdd51d7
Joshua Cruz <jcruz1998@gmail.com>
2024-05-26 17:32:19 -0400

    Adds myfild.md

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainElement = document.querySelector("#main");
+mainElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Josh is the champion";
+document.body.appendChild(newHeader);
